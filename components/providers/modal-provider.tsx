ea9c88367d317c029cc0cea6fb6ac87f5b488b37
Theknight <himanshusaxena5005@gmail.com>
2024-07-13 14:00:53 +0530

    Added functionality for changing user role and kick them out from the group

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import CreateServerModal from "../modals/create-server-modal";
 import InviteModal from "../modals/invite-modal";
 import EditServerModal from "../modals/edit-server-modal";
-import MembersModal from "../modals/manage-member-modal";
+import { MembersModal } from "../modals/manage-member-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
