53f79ec69ec4940dfe225aa063967d93bc7828a3
Theknight <himanshusaxena5005@gmail.com>
2024-08-21 15:34:48 +0530

    final

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -32,7 +32,7 @@ export const ServerMembers = ({ member, server }: ServerMemberProps) => {
       onClick={onClick}
       className={cn(
         "group px-2  py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-        params.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
       )}
     >
       <UserAvatar
