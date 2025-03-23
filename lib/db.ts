cfcac653e012706bbfec73744e66c82a9e49e018
Himanshu Saxena <119810076+Theknight-1@users.noreply.github.com>
2025-03-23 18:31:08 +0530

    Initial redirection to discovery page if no server found

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma : PrismaClient | undefined
-}
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma || new PrismaClient();
 
-if(process.env. NODE_ENV !== "production") globalThis.prisma == db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
